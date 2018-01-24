c17593109a36ae4a0fae6c7d3c5a7ce50aed56a9
Jared H <35547112+JedHarb@users.noreply.github.com>
2018-01-23 18:48:47 -0600

    Project 2 Page 37
    Project 2 Page 37 for makeup work. I used a tab at the start of each line and all spaces besides that, it's the only way I could get all the text to line up the way I need to.

---
+++ Lab2Makeup.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "*************************************************\n";
+	cout << "\n";
+	cout << "	   ccc            ssss      !!\n";
+	cout << "	  c   c          s    s     !!\n";
+	cout << "	 c              s           !!\n";
+	cout << "	c                s          !!\n";
+	cout << "	c                 ssss      !!\n";
+	cout << "	c                     s     !!\n";
+	cout << "	 c                     s    !!\n";
+	cout << "	  c   c         s     s\n";
+	cout << "	   ccc            ssss      00\n";
+	cout << "\n";
+	cout << "*************************************************\n";
+	cout << "	Computer Science is Cool Stuff!";
+	
+	return 0;
+}
